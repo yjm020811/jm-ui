5e23af3e20decf4a5d23258660e238670db41977
yjm020811 <2806654030@qq.com>
2024-08-31 10:05:05 +0800

    处理table与form嵌套dialog的情况

--- src/components/myForm/index.vue
+++ src/components/myForm/index.vue
@@ -1,102 +1,167 @@
 <template>
-    <el-form ref="form" v-if="model" :validate-on-rule-change="false" :rules="rules" :model="model" v-bind="$attrs">
-        <el-row :gutter="20">
-            <template v-for="(item, index) in props.formOptions" :key="index">
-                <el-col :span="item.colSpan || 12">
-                    <el-form-item v-if="!item.children || !item.children.length" :prop="item.prop" :label="item.label">
-                        <!-- 非上传且不是插槽内容 -->
-                         <!-- 注意：本项目采用的全局注册elementPlus组件，所以这里的动态组件能够正常渲染出来，当项目采用的局部注册就需要在main.js中单独处理并导入utils中的函数 -->
-                        <component v-if="item.type !== 'upload' && item.type !== 'slot'" :placeholder="item.placeholder" v-bind="item.attrs"
-                            :is="`el-${item.type}`" v-model="model[item.prop!]"></component>
-                        <!-- 上传 -->
-                        <el-upload multiple v-if="item.type === 'upload'" v-bind="item.uploadAttrs"
-                            :on-preview="onPreview" :on-remove="onRemove" :on-success="onSuccess" :on-error="onError"
-                            :on-progress="onProgress" :on-change="onChange" :before-upload="beforeUpload"
-                            :before-remove="beforeRemove" :http-request="httpRequest" :on-exceed="onExceed">
-                            <slot name="uploadArea"></slot>
-                            <slot name="uploadTip"></slot>
-                        </el-upload>
-                        <!-- 自定义插槽 -->
-                        <slot v-if="item.type === 'slot'" :name="item.slotName" :model="model"></slot>
-                    </el-form-item>
-                    <!-- 子项处理 -->
-                    <el-form-item v-if="item.children && item.children.length" :prop="item.prop" :label="item.label">
-                        <component :placeholder="item.placeholder" v-bind="item.attrs" :is="`el-${item.type}`"
-                            v-model="model[item.prop!]">
-                            <component v-for="(child, i) in item.children" :key="i" :is="`el-${child.type}`"
-                                :label="child.label" :value="child.value">
-                            </component>
-                        </component>
-                    </el-form-item>
-                </el-col>
-            </template>
-        </el-row>
-        <!-- 操作项 -->
-        <el-form-item>
-            <slot name="action" :form="form" :model="model"></slot>
-        </el-form-item>
-    </el-form>
+  <el-form
+    ref="form"
+    v-if="model"
+    :validate-on-rule-change="false"
+    :rules="rules"
+    :model="model"
+    v-bind="$attrs"
+  >
+    <el-row :gutter="20">
+      <template v-for="(item, index) in props.formOptions" :key="index">
+        <el-col :span="item.colSpan || 12">
+          <el-form-item
+            v-if="!item.children || !item.children.length"
+            :prop="item.prop"
+            :label="item.label"
+          >
+            <!-- 非上传且不是插槽内容 -->
+            <!-- 注意：本项目采用的全局注册elementPlus组件，所以这里的动态组件能够正常渲染出来，当项目采用的局部注册就需要在main.js中单独处理并导入utils中的函数 -->
+            <component
+              v-if="item.type !== 'upload' && item.type !== 'slot'"
+              :placeholder="item.placeholder"
+              v-bind="item.attrs"
+              :is="`el-${item.type}`"
+              v-model="model[item.prop!]"
+            ></component>
+            <!-- 上传 -->
+            <el-upload
+              multiple
+              v-if="item.type === 'upload'"
+              v-bind="item.uploadAttrs"
+              :on-preview="onPreview"
+              :on-remove="onRemove"
+              :on-success="onSuccess"
+              :on-error="onError"
+              :on-progress="onProgress"
+              :on-change="onChange"
+              :before-upload="beforeUpload"
+              :before-remove="beforeRemove"
+              :http-request="httpRequest"
+              :on-exceed="onExceed"
+            >
+              <slot name="uploadArea"></slot>
+              <slot name="uploadTip"></slot>
+            </el-upload>
+            <!-- 自定义插槽 -->
+            <slot
+              v-if="item.type === 'slot'"
+              :name="item.slotName"
+              :model="model"
+            ></slot>
+          </el-form-item>
+          <!-- 子项处理 -->
+          <el-form-item
+            v-if="item.children && item.children.length"
+            :prop="item.prop"
+            :label="item.label"
+          >
+            <component
+              :placeholder="item.placeholder"
+              v-bind="item.attrs"
+              :is="`el-${item.type}`"
+              v-model="model[item.prop!]"
+            >
+              <component
+                v-for="(child, i) in item.children"
+                :key="i"
+                :is="`el-${child.type}`"
+                :label="child.label"
+                :value="child.value"
+              >
+              </component>
+            </component>
+          </el-form-item>
+        </el-col>
+      </template>
+    </el-row>
+    <!-- 操作项 -->
+    <div style="display: flex; justify-content: flex-end">
+      <slot name="action" :form="form" :model="model"></slot>
+    </div>
+  </el-form>
 </template>
-<script lang='ts' setup>
-import { ref, onMounted, watch, PropType } from 'vue'
-import { FormInstance, FormOptions } from './types/types'
+<script lang="ts" setup>
+import { ref, onMounted, watch, PropType } from "vue";
+import { FormInstance, FormOptions } from "./types/types";
 const props = defineProps({
-    // 表单的配置选项
-    formOptions: {
-        type: Array as PropType<FormOptions[]>,
-        required: true
-    },
-    // 用户自定义上传方法
-    httpRequest: {
-        type: Function
-    },
-})
+  // 表单的配置选项
+  formOptions: {
+    type: Array as PropType<FormOptions[]>,
+    required: true
+  },
+  // 用户自定义上传方法
+  httpRequest: {
+    type: Function
+  }
+});
 
 // 表单数据
-const model = ref(null)
-const rules = ref(null)
-const form = ref<FormInstance | null>()
+const model = ref(null);
+const rules = ref(null);
+const form = ref<FormInstance | null>();
 
 const initForm = () => {
-    if (props.formOptions && props.formOptions.length) {
-        let m = {}
-        let r = {}
-        props.formOptions.map((item: FormOptions) => {
-            m[item.prop!] = item.value
-            r[item.prop!] = item.rules
-        })
-        model.value = JSON.parse(JSON.stringify(m))
-        rules.value = JSON.parse(JSON.stringify(r))
-    }
-}
+  if (props.formOptions && props.formOptions.length) {
+    let m = {};
+    let r = {};
+    props.formOptions.map((item: FormOptions) => {
+      m[item.prop!] = item.value;
+      r[item.prop!] = item.rules;
+    });
+    model.value = JSON.parse(JSON.stringify(m));
+    rules.value = JSON.parse(JSON.stringify(r));
+  }
+};
 
 onMounted(() => {
-    initForm()
-})
+  initForm();
+});
 
 // 监听props.formOptions的变化，重新初始化表单
-watch(() => props.formOptions, () => {
-    initForm()
-}, { deep: true })
+watch(
+  () => props.formOptions,
+  () => {
+    initForm();
+  },
+  { deep: true }
+);
 
-const emits = defineEmits(["on-preview", "on-remove", "on-success", "on-error", "on-progress", "on-change", "before-upload", "before-remove", "on-exceed"])
+const emits = defineEmits([
+  "on-preview",
+  "on-remove",
+  "on-success",
+  "on-error",
+  "on-progress",
+  "on-change",
+  "before-upload",
+  "before-remove",
+  "on-exceed"
+]);
 
 // 上传组件事件处理方法
-const onPreview = (file: any) => emits("on-preview", file)
-const onRemove = (file: any, fileList: any) => emits("on-remove", { file, fileList })
+const onPreview = (file: any) => emits("on-preview", file);
+const onRemove = (file: any, fileList: any) =>
+  emits("on-remove", { file, fileList });
 const onSuccess = (response: any, file: any, fileList: any) => {
-    let uploadItem = props.formOptions.find((item: any) => item.type === "upload")
-    if (uploadItem) {
-        (model.value as any)[uploadItem.prop!] = { response, file, fileList }
-    }
-    emits("on-success", { response, file, fileList })
-}
-const onError = (err: any, file: any, fileList: any) => emits("on-error", { err, file, fileList })
-const onProgress = (event: any, file: any, fileList: any) => emits("on-progress", { event, file, fileList })
-const onChange = (file: any, fileList: any) => emits("on-change", { file, fileList })
-const beforeUpload = (file: any) => emits("before-upload", file)
-const beforeRemove = (file: any, fileList: any) => emits("before-remove", { file, fileList })
-const onExceed = (files: any, fileList: any) => emits("on-exceed", { files, fileList })
-
+  let uploadItem = props.formOptions.find(
+    (item: any) => item.type === "upload"
+  );
+  if (uploadItem) {
+    (model.value as any)[uploadItem.prop!] = { response, file, fileList };
+  }
+  emits("on-success", { response, file, fileList });
+};
+const onError = (err: any, file: any, fileList: any) =>
+  emits("on-error", { err, file, fileList });
+const onProgress = (event: any, file: any, fileList: any) =>
+  emits("on-progress", { event, file, fileList });
+const onChange = (file: any, fileList: any) =>
+  emits("on-change", { file, fileList });
+const beforeUpload = (file: any) => emits("before-upload", file);
+const beforeRemove = (file: any, fileList: any) =>
+  emits("before-remove", { file, fileList });
+const onExceed = (files: any, fileList: any) =>
+  emits("on-exceed", { files, fileList });
 </script>
-<style lang='scss' scoped></style>
+<style lang="scss" scoped></style>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,7 +1,14 @@
-<script setup>
+<script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import axios from "axios";
 import dayjs from "dayjs";
+import { FormInstance, FormOptions } from "../../components/myForm/types/types";
+import { ElMessage } from "element-plus";
+
+interface Scope {
+  form: FormInstance;
+  model: any;
+}
 
 //el-table的data数据
 const tableData = ref([]);
@@ -12,25 +19,25 @@ const pageSize = ref(10);
 // 总数
 const total = ref(null);
 
-const getData = () => {
-  // 构造请求的URL和参数  
+const getData = (...args: any) => {
+  // 构造请求的URL和参数
   const url = "http://localhost:3000/my/activity/allActivity";
   const params = {
     limit: pageSize.value,
     page: currentPage.value
   };
 
-  // 使用axios发送GET请求，通过params对象传递查询参数  
-  axios.get(url, { params })
+  // 使用axios发送GET请求，通过params对象传递查询参数
+  axios
+    .get(url, { params })
     .then((res) => {
       console.log(res.data.data);
       tableData.value = res.data.data;
       total.value = res.data.total;
-
     })
     .catch((error) => {
-      // 处理错误情况  
-      console.error('There was an error!', error);
+      // 处理错误情况
+      console.error("There was an error!", error);
     });
 };
 onMounted(() => {
@@ -39,16 +46,16 @@ onMounted(() => {
 
 // 每页多少条数据
 let handleSizeChange = (val) => {
-  console.log('当前一页多少条数据', val)
+  console.log("当前一页多少条数据", val);
   pageSize.value = val;
   getData();
 };
 
 // 在那一页
 let handleCurrentChange = (val) => {
-  console.log("当前在哪一页", val)
+  console.log("当前在哪一页", val);
   currentPage.value = val;
-  getData()
+  getData();
 };
 
 //el-table-column配置
@@ -88,6 +95,15 @@ const actionOptions = {
 // 编辑表格数据
 const edit = (scope) => {
   console.log(`编辑表格`, scope.row);
+  dialogVisible.value = true;
+  // 获取scope.row的数据类型，遍历formOptions，添加到对应的prop的value
+  for (let key in scope.row) {
+    for (let i = 0; i < formOptions.length; i++) {
+      if (formOptions[i].prop === key) {
+        formOptions[i].value = scope.row[key];
+      }
+    }
+  }
 };
 
 // 点击勾
@@ -104,6 +120,79 @@ const cancel = (scope) => {
 const deleteAction = (scope) => {
   console.log(`删除表格`, scope.row);
 };
+
+const formOptions: FormOptions[] = [
+  {
+    label: "活动id",
+    prop: "id",
+    type: "input",
+    placeholder: "请输入活动id",
+    value: "",
+    rules: [{ required: true, message: "请输入活动id", trigger: "blur" }],
+    attrs: {
+      clearable: true
+    }
+  },
+  {
+    label: "活动描述",
+    prop: "activityDesc",
+    type: "input",
+    placeholder: "请输入活动描述",
+    value: "",
+    rules: [
+      { required: true, message: "请输入活动描述", trigger: "blur" },
+      { min: 2, max: 10, message: "长度在 2 到 10 个字符", trigger: "blur" }
+    ],
+    attrs: {
+      clearable: true
+    }
+  },
+  {
+    label: "活动名称",
+    prop: "activityName",
+    type: "input",
+    placeholder: "请输入活动名称",
+    value: "",
+    rules: [
+      { required: true, message: "请输入活动名称", trigger: "blur" },
+      { min: 2, max: 10, message: "长度在 2 到 10 个字符", trigger: "blur" }
+    ],
+    attrs: {
+      clearable: true
+    }
+  },
+  {
+    type: "date-picker",
+    label: "日期",
+    prop: "activityStartTime",
+    // 需要value显示今天
+    value: "",
+    rules: [{ required: true, message: "请选择日期", trigger: "change" }],
+    attrs: {
+      style: {
+        width: "300px"
+      }
+    }
+  }
+];
+const dialogVisible = ref(false);
+const onSubmit = (scope: Scope) => {
+  scope.form.validate((valid: any) => {
+    if (valid) {
+      console.log(scope.model);
+      ElMessage.success("提交成功");
+    } else {
+      ElMessage.error("表单填写有误");
+      return false;
+    }
+  });
+};
+
+// 表单重置
+const reset = (scope: any) => {
+  scope.form.resetFields();
+  dialogVisible.value = false;
+};
 </script>
 
 <template>
@@ -117,20 +206,20 @@ const deleteAction = (scope) => {
       }}</span>
     </template>
 
-    <!-- 自定义编辑插槽 -->
-    <!-- <template #editCell="{ scope }">
-      <div style="display: flex;margin-left: 8px;">
-        <el-button size="small" type="primary">点击</el-button>
-        <el-button size="small" type="danger">取消</el-button>
-      </div>
-    </template> -->
-
     <!-- 自定义编辑区域 -->
     <template #action="{ scope }">
       <el-button size="small" type="primary" @click="edit(scope)">编辑</el-button>
       <el-button size="small" type="danger" @click="deleteAction(scope)">删除</el-button>
     </template>
   </MyTable>
+  <el-dialog destroy-on-close v-model="dialogVisible" title="编辑活动信息">
+    <MyForm :formOptions="formOptions">
+      <template #action="scope">
+        <el-button type="primary" @click="onSubmit(scope)">提交</el-button>
+        <el-button @click="reset(scope)">关闭</el-button>
+      </template>
+    </MyForm>
+  </el-dialog>
 </template>
 <style scoped>
 svg {
